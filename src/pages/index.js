094931a976291bdd1025393f4b6df62bd5cbbc69
Igor Vinicius <igorvinicius_22@hotmail.com>
2021-11-16 15:32:49 -0300

    Material-UI cinfiguration

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Button } from '@material-ui/core';
 
 export default function Home() {
   return (
     <div>
       Clone Youtube com Next.JS + Material-UI
-    </div>
-  )
+      <Button variant="outlined" color="secondary">
+        Primary
+      </Button>
+      </div>
+  );
 }
