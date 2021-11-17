e4b8bb22b41f3930321140db00d5169a88190023
Igor Vinicius <igorvinicius_22@hotmail.com>
2021-11-17 13:48:24 -0300

    Layout Component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Button } from '@material-ui/core';
 
+import Layout from '../components/Layout';
+
 export default function Home() {
   return (
-    <div>
+    <Layout title="AcconTube">
       Clone Youtube com Next.JS + Material-UI
       <Button variant="outlined" color="secondary">
         Primary
       </Button>
-      </div>
+      </Layout>
   );
 }
